d1d649b8830e0aa0ce0bc9815165e8fd61e8c046
Artur Jamro <artur.jamro@gmail.com>
2019-04-13 00:56:30 +0200

    mzad4 done (hello chrdev)

--- mzad4/zso5_chrdev.c
+++ mzad4/zso5_chrdev.c
@@ -5,16 +5,18 @@
 #include <linux/uaccess.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/cred.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 
 #define HELLO_IOCTL_SET_REPEATS _IO('H', 0x00)
 #define HELLO_MAX_REPEATS 0x100
 #define HELLO_MAX_WRITE_BYTES 32
+#define HELLO_DEFAULT_REPEATS 1
 
 static const char hello_reply[] = "Hello, world!\n";
 static size_t hello_len = sizeof hello_reply - 1;
-static long hello_repeats = 1;
 static dev_t hello_major;
 static struct cdev hello_cdev;
 static struct cdev hello_once_cdev;
@@ -24,6 +26,84 @@ static struct class hello_class = {
 };
 static struct device *hello_device;
 static struct device *hello_once_device;
+
+
+typedef struct repeats_map {
+	struct list_head list;
+	uid_t uid;
+	long repeats;
+} repeats_map_t;
+
+static repeats_map_t hello_repeats_map = {
+	.list = LIST_HEAD_INIT(hello_repeats_map.list),
+	.uid = 0, // init for root
+	.repeats = HELLO_DEFAULT_REPEATS,
+};
+
+static DEFINE_MUTEX(hello_repeats_mutex);
+
+static ssize_t hello_repeats_set(uid_t user, long value) {
+	repeats_map_t *pos = NULL;
+	repeats_map_t *new_elem = NULL;
+	int found = 0;
+	ssize_t result = 0;
+
+	mutex_lock(&hello_repeats_mutex);
+	list_for_each_entry(pos, &hello_repeats_map.list, list) {
+		if (pos->uid == user) {
+			pos->repeats = value;
+			found = 1;
+			break;
+		}
+	}
+	if (!found && value != HELLO_DEFAULT_REPEATS) {
+		new_elem = kmalloc(sizeof(repeats_map_t), GFP_ATOMIC);
+		if (new_elem == NULL) {
+			result = -ENOMEM;
+		}
+		else {
+			INIT_LIST_HEAD(&new_elem->list);
+			new_elem->uid = user;
+			new_elem->repeats = value;
+			list_add_tail(&new_elem->list, &hello_repeats_map.list);
+		}
+	}
+	mutex_unlock(&hello_repeats_mutex);
+
+	return result;
+}
+
+static long hello_repeats_get(uid_t user) {
+	repeats_map_t *pos = NULL;
+	long result = HELLO_DEFAULT_REPEATS;
+
+	mutex_lock(&hello_repeats_mutex);
+	list_for_each_entry(pos, &hello_repeats_map.list, list) {
+		if (pos->uid == user) {
+			result = pos->repeats;
+			break;
+		}
+	}
+	mutex_unlock(&hello_repeats_mutex);
+
+	return result;
+}
+
+static void hello_repeats_cleanup(void) {
+	repeats_map_t *pos = NULL;
+	repeats_map_t *n = NULL;
+	struct list_head *head = &hello_repeats_map.list;
+
+	mutex_lock(&hello_repeats_mutex);
+	list_for_each_entry_safe(pos, n, head, list) {
+		if (&pos->list != head) {
+			list_del(&pos->list);
+			kfree(pos);
+		}
+	}
+	mutex_unlock(&hello_repeats_mutex);
+}
+
 
 static ssize_t hello_once_read(struct file *file, char __user *buf, size_t count, loff_t *filepos)
 {
@@ -40,7 +120,8 @@ static ssize_t hello_once_read(struct file *file, char __user *buf, size_t count
 
 static ssize_t hello_read(struct file *file, char __user *buf, size_t count, loff_t *filepos)
 {
-	size_t file_len = hello_len * hello_repeats;
+	uid_t uid = __kuid_val(task_uid(current));
+	size_t file_len = hello_len * hello_repeats_get(uid);
 	loff_t pos = *filepos;
 	loff_t end;
 	if (pos >= file_len || pos < 0)
@@ -57,9 +138,10 @@ static ssize_t hello_read(struct file *file, char __user *buf, size_t count, lof
 
 static ssize_t hello_write(struct file *filp, const char __user *buff, size_t count, loff_t *offp) {
 	char buffer[HELLO_MAX_WRITE_BYTES] = {0};
-	long old_repeats = 0;
 	long repeats = 0;
 	size_t it = 0;
+	uid_t uid;
+	ssize_t err_code;
 	
 	if (count > HELLO_MAX_WRITE_BYTES) {
 		return -EINVAL;
@@ -76,26 +158,29 @@ static ssize_t hello_write(struct file *filp, const char __user *buff, size_t co
 		else if (buffer[it] < '0' || '9' < buffer[it]) {
 			return -EINVAL;
 		}
-		old_repeats = repeats;
 		repeats = repeats * 10 + (buffer[it] - '0');
-		if (repeats < old_repeats) { // overflow
+		if (HELLO_MAX_REPEATS < repeats) {
 			return -EINVAL;
 		}
 	}
 
-	hello_repeats = repeats;
-	return count;
+	uid = __kuid_val(task_uid(current)); // no need to read it before (uses rcu lock)
+	err_code = hello_repeats_set(uid, repeats);
+
+	return err_code != 0 ? err_code : count;
 }
 
-// static long hello_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-// {
-// 	if (cmd != HELLO_IOCTL_SET_REPEATS)
-// 		return -ENOTTY;
-// 	if (arg > HELLO_MAX_REPEATS)
-// 		return -EINVAL;
-// 	hello_repeats = arg;
-// 	return 0;
-// }
+static long hello_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	uid_t uid;
+	if (cmd != HELLO_IOCTL_SET_REPEATS)
+		return -ENOTTY;
+	if (arg > HELLO_MAX_REPEATS)
+		return -EINVAL;
+
+	uid = __kuid_val(task_uid(current)); // no need to read it before (uses rcu lock)
+	return hello_repeats_set(uid, arg);
+}
 
 static int hello_open(struct inode *ino, struct file *filep);
 static int hello_release(struct inode *ino, struct file *filep);
@@ -112,8 +197,8 @@ static struct file_operations hello_fops = {
 	.read = hello_read,
 	.write = hello_write,
 	.open = hello_open,
-	// .unlocked_ioctl = hello_ioctl,
-	// .compat_ioctl = hello_ioctl,
+	.unlocked_ioctl = hello_ioctl,
+	.compat_ioctl = hello_ioctl,
 	.release = hello_release,
 };
 
@@ -169,6 +254,7 @@ err_alloc:
 
 static void hello_cleanup(void)
 {
+	hello_repeats_cleanup();
 	device_destroy(&hello_class, hello_major + 1);
 	device_destroy(&hello_class, hello_major);
 	class_unregister(&hello_class);
